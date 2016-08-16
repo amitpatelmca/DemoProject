ad37af1d903c8b9bcedf17c943f2b52127ca3667
amitpatelmca <amitpatelmca@gmail.com>
2016-08-15 22:41:29 -0400

    First Save

---
+++ mvcMusicStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mvcMusicStore.Startup))]
+namespace mvcMusicStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
